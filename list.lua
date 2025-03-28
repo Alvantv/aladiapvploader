b30c70b084e751481636a26e421f57f90d116194
Vermilion <108969992+Alvantv@users.noreply.github.com>
2025-03-28 13:06:54 +0700

    Create list.lua

---
+++ list.lua
@@ -0,0 +1,4 @@
+Usn: Henclin | Key: 111 | Exp: 01/01/3000
+Usn: KemilingHUB2 | Key: 1111 | Exp: 01/01/3000
+Usn: DanzzRBX | Key: danigblk | Exp: 09/03/2025
+Usn: justinhgo21 | Exp: jambicoli | Exp: 09/03/2025
