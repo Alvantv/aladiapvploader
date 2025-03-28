d07600c3cb083faa6cfc5479a9f815602a2ce728
Vermilion <108969992+Alvantv@users.noreply.github.com>
2025-03-28 13:30:54 +0700

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,4 +1,15 @@
-Usn: Henclin | Key: 111 | Exp: 01/01/3000
+-- Admin
+Usn: Henclin | Key: 1111 | Exp: 01/01/3000
 Usn: KemilingHUB2 | Key: 1111 | Exp: 01/01/3000
+Usn: Janggaliwir | Key: 1111 | Exp: 01/01/3000
+
+-- Free (Premium) user
 Usn: justinhgo21 | Key: danigblk | Exp: 09/03/2025 23:59
 Usn: DanzzRBX | Key: jambicoli | Exp: 09/03/2025 23:59
+
+
+-- Buyer
+
+
+-- Note User
+Usn: ATHONKGAMING | Key: ucNXxhceLy | Note: SEKARANG SIRKEL YE
