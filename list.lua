ce2d82bc738587cf305e3551cbeb7007d8820245
Vermilion <108969992+Alvantv@users.noreply.github.com>
2025-03-28 20:51:18 +0700

    Update list.lua

--- list.lua
+++ list.lua
@@ -9,10 +9,12 @@ Usn: DanzzRBX | Key: jambicoli | Exp: 09/03/2025 23:59
 
 -- Buyer
 
+-- Nbuy User
+Usn: UsernameHere | Nbuy: true | Note: (Optional)
+Usn: Henclin | Nbuy: true
 
 -- Note User
 Usn: ATHONKGAMING | Key: ucNXxhceLy | Note: JAWA JAWA JAWA
 
 -- Blacklist
 Usn: DODIKANJUTT | Blacklist: true
-Usn: Henclin | Nbuy: True
