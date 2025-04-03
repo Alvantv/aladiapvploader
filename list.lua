975a7e89faf0350802514782941d859ad8b7a6d7
Vermilion <108969992+Alvantv@users.noreply.github.com>
2025-04-04 00:04:01 +0700

    Update list.lua

--- list.lua
+++ list.lua
@@ -11,13 +11,13 @@ Usn: sevshtz | Key: jxXepQ5XR | Exp: 1/04/2025 12:57
 
 -- Nbuy User
 Usn: UsernameHere | Nbuy: true | Note: (Optional)
+Usn: DODIKANJUTT | Nbuy: true | Note: buy anjg
 
 -- Note User
 Usn: ATHONKGAMING | Key: ucNXxhceLy | Note: JAWA JAWA JAWA
 
 -- Blacklist
 Usn: Henclin | Blacklist: true
-Usn: DODIKANJUTT | Blacklist: true
 
 -- Blacklist (Admin Ingame)
 Usn: niohailhad | Blacklist: true
