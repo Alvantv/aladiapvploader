341097a6a9bdd8d9a1acaee470c0804c29197438
Vermilion <108969992+Alvantv@users.noreply.github.com>
2025-04-03 12:42:41 +0700

    Update list.lua

--- list.lua
+++ list.lua
@@ -1,10 +1,7 @@
 -- Admin KemilingHUB
-Usn: Henclin | Key: 1111 | Perm: True
-Usn: KemilingHUB2 | Key: 1111 | Perm: True
-Usn: Janggaliwir | Key: 1111 | Perm: True
-
--- Permanent User
-Usn: Test123 | Key: 1111 | Perm: True
+Usn: Henclin | Key: 1111 | Exp: 01/01/3000
+Usn: KemilingHUB2 | Key: 1111 | Exp: 01/01/3000
+Usn: Janggaliwir | Key: 1111 | Exp: 01/01/3000
 
 -- Free (Premium) user
 Usn: DanzzRBX | Key: 102009 | Exp: 01/01/3000
